c69c494bfb32eba8adfe30ee8ae93b280e7c58a9
JAEHO SONG <thruthesky@gmail.com>
2018-10-10 22:31:49 +0800

    update config

--- db.config.php
+++ db.config.php
@@ -2,7 +2,7 @@
 include_once "ezSQL/shared/ez_sql_core.php";
 include_once "ezSQL/lib/ez_sql_mysqli.php";
 
-$conn = new mysqli('localhost', 'root', '7777', 'sonub', 0, '/tmp/mysql.sock');
+$conn = new mysqli('localhost', 'root', '7777', '_sonub', 0, '/tmp/mysql.sock');
 $db = new ezSQL_mysqli();
 $db->dbh = $conn;
 
