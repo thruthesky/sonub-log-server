f4b6a7ca8068f1287b17d8b00f9cf2879f324c07
lancelynyrd <pinedaclp@gmail.com>
2018-10-11 15:42:39 +0800

    db.config

--- db.config.php
+++ db.config.php
@@ -2,8 +2,9 @@
 include_once "ezSQL/shared/ez_sql_core.php";
 include_once "ezSQL/lib/ez_sql_mysqli.php";
 
-$conn = new mysqli('localhost', 'root', '7777', 'sonub', 0, '/tmp/mysql.sock');
-$db = new ezSQL_mysqli();
-$db->dbh = $conn;
+//$conn = new mysqli('localhost', 'root', '7777', 'sonub', 0, '/tmp/mysql.sock');
+//$conn = new mysqli('localhost', 'root', '7777', '_sonub');
+$db = new ezSQL_mysqli('root', '7777', '_sonub');
+//$db->dbh = $conn;
 
 
