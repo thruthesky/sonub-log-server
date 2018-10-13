3b9cd250896ab816931c540af0b1a5e6298f0f42
Your Name <you@example.com>
2018-10-13 13:11:09 +0800

    database confirg

--- db.config.php
+++ db.config.php
@@ -2,8 +2,8 @@
 include_once "ezSQL/shared/ez_sql_core.php";
 include_once "ezSQL/lib/ez_sql_mysqli.php";
 
-$conn = new mysqli('localhost', 'root', '7777', '_sonub', 0, '/tmp/mysql.sock');
-$db = new ezSQL_mysqli();
-$db->dbh = $conn;
+//$conn = new mysqli('localhost', 'root', '7777', '_sonub', 0, '/tmp/mysql.sock');
+$db = new ezSQL_mysqli('_sonub', 'Wc~045s$5', '_sonub');
+//$db->dbh = $conn;
 
 
